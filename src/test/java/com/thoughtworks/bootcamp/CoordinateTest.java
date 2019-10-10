6f66d1dff88ad73be639fee0233bff9337b87e05
snehamanojraopalekar <nehapalekar026@gmail.com>
2019-10-10 13:42:25 +0530

    Add. coordinate 7,3 when is in Between boundary then should return false

--- src/test/java/com/thoughtworks/bootcamp/CoordinateTest.java
+++ src/test/java/com/thoughtworks/bootcamp/CoordinateTest.java
@@ -6,9 +6,16 @@ import org.junit.jupiter.api.Test;
 class CoordinateTest {
 
     @Test
-    void givenCoordinate_WhenBetweenThenBoundary_ThenShouldReturnTrue() {
+    void givenCoordinateTwoAndThree_WhenInBetweenThenBoundary_ThenShouldReturnTrue() {
         Coordinate upperCoordinate = new Coordinate(5, 5);
         Coordinate coordinate = new Coordinate(2, 3);
         Assertions.assertTrue(coordinate.isBetween(upperCoordinate));
+    }
+
+    @Test
+    void givenCoordinateSevenAndThree_WhenInBetweenThenBoundary_ThenShouldReturnTrue() {
+        Coordinate upperCoordinate = new Coordinate(5, 5);
+        Coordinate coordinate = new Coordinate(7, 3);
+        Assertions.assertFalse(coordinate.isBetween(upperCoordinate));
     }
 }
